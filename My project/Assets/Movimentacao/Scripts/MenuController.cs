19951ce04691c210e53ad53622c1a8e1edd43d61
Andreza Leal <mirelyrusher@gmail.com>
2024-05-31 13:43:27 -0300

    Tela inicial
    tela inicial com componentes funcionando, falta o nome do jogo

--- My project/Assets/Movimentacao/Scripts/MenuController.cs
+++ My project/Assets/Movimentacao/Scripts/MenuController.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+{   
+    [SerializeField] private string nomeDoLevelDoJogo;
+    [SerializeField] private GameObject painelMenuInicial;
+    [SerializeField] private GameObject painelOpcoes;
+
+    public void Jogar() {
+
+        SceneManager.LoadScene(nomeDoLevelDoJogo);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void AbrirOpcoes() {
+
+        painelMenuInicial.SetActive(false);
+        painelOpcoes.SetActive(true);
+    }
+
+    public void FecharOpcoes() {
+
+        painelOpcoes.SetActive(false);
+        painelMenuInicial.SetActive(true);
+    }
+
+    public void SairJogo(){
+        Debug.Log("Sair do jogo");
+        Application.Quit();
     }
 }
